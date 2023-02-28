cede6ceeecdfea30a2916229d4f328f0e347ef70
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-02-28 15:03:27 +0100

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # A regex expression to that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/Holberton/).join
+puts ARGV[0].scan(/School/).join
