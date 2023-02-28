f1ea275527ffdb1150d4d17afc0c1bd1d1b98522
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-02-28 15:12:58 +0100

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #A Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/[hbt+n]/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
