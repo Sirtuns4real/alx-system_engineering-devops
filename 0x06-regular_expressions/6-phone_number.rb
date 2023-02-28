b01b19b3e6442abafd6b5eb3a3d89fc90e5a8396
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-02-28 15:59:29 +0100

    task 6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # , create a Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/^\d{1,4}\s?-?+\d{1,3}\s?-?\d{1,4}$/).join
+puts ARGV[0].scan(/^\d{10,10}$/).join
