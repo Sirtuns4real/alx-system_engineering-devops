737b073633ea483aca5ce51707ac4215f7b9a3bb
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-02-28 16:01:41 +0100

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# , create a Ruby script that accepts one argument and pass it to a regular expression matching method
+#create a Ruby script that accepts one argument and pass it to a regular expression matching method
 
 puts ARGV[0].scan(/^\d{10,10}$/).join
