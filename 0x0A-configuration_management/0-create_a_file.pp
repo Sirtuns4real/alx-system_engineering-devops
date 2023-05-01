ec23183b7f163ec6175638ed16bd22b44061aae5
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-01 12:40:02 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file in tmp
 
 file { '/tmp/holberton':
-  ensure => file,
-  path => '/tmp/holberton',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
