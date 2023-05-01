d184c8ff5a7ff07d33c7023c561bd79aca93700a
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-01 12:52:49 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs puppet-lint, version 2.1.1
+# Installs flask, version 2.1.0.
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
