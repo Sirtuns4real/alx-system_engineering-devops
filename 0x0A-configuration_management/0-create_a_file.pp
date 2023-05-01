686f426255291ca5c7a04371d93ab77e424e7dd0
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-01 12:53:58 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file in tmp
 
-file { '/tmp/holberton':
+file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
