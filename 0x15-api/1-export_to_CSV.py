9b1e96fe9fe6bc16de1a05534a2b5f779c36578b
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2023-05-23 02:30:41 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that exports to-do list info for a given employee ID to CSV format."""
+"""Script to export to-do list info for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
